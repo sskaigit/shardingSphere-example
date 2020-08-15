6dd6d3f25a8e5968be839c69b9ce35724ef908f8
邵世凯 <shao.sk@burgeon.cn>
2020-08-16 02:34:53 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 
 @SpringBootApplication(exclude = JtaAutoConfiguration.class)
 public class DemoApplication {
